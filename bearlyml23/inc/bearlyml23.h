701b7d9b1ce639154126d60297e517e025c1b722
Jason Kim <jasonskim007@berkeley.edu>
2023-10-19 11:37:41 -0700

    fixed bugs and memory structs

--- bearlyml23/inc/bearlyml23.h
+++ bearlyml23/inc/bearlyml23.h
@@ -92,8 +92,7 @@ typedef struct {
   __IO uint32_t MSIP1;                          /** MSIP Registers (1 bit wide) */
   __IO uint32_t MSIP2;                          /** MSIP Registers (1 bit wide) */
   __IO uint32_t MSIP3;                          /** MSIP Registers (1 bit wide) */
-  __IO uint32_t MSIP4;                          /** MSIP Registers (1 bit wide) */
-  uint32_t RESERVED0[4091];
+  uint32_t RESERVED0[4092];
   __IO uint64_t MTIMECMP0;                      /** MTIMECMP Registers */
   uint32_t RESERVED1[8188];
   __IO uint64_t MTIME;                          /** Timer Register */
@@ -122,26 +121,23 @@ typedef struct {
   __IO uint32_t TILE1_RESET;      /** RocketTile 1 */   // 0x04
   __IO uint32_t TILE2_RESET;      /** RocketTile 2 */   // 0x08
   __IO uint32_t TILE3_RESET;      /** RocketTile 3 */   // 0x0C
-  __IO uint32_t TILE4_RESET;      /** SaturnTile 0 */   // 0x10
-  uint32_t RESERVED0[1021];
-  __IO uint32_t TILE0_CLKGATE;    /** RocketTile 0 */   // 0x1008
-  __IO uint32_t TILE1_CLKGATE;    /** RocketTile 1 */   // 0x100C
-  __IO uint32_t TILE2_CLKGATE;    /** RocketTile 2 */   // 0x1010
-  __IO uint32_t TILE3_CLKGATE;    /** RocketTile 3 */   // 0x1014
-  __IO uint32_t TILE4_CLKGATE;    /** SaturnTile 4 */   // 0x1018
-  uint32_t RESERVED1[1017];
+  uint32_t RESERVED0[1020];
+  __IO uint32_t TILE0_CLKGATE;    /** RocketTile 0 */   // 0x1000
+  __IO uint32_t TILE1_CLKGATE;    /** RocketTile 1 */   // 0x1004
+  __IO uint32_t TILE2_CLKGATE;    /** RocketTile 2 */   // 0x1008
+  __IO uint32_t TILE3_CLKGATE;    /** RocketTile 3 */   // 0x100C
+  uint32_t RESERVED1[1020];
   __IO uint32_t CLK_SEL;          /** Selects the system clock. 0: External slow clock, 1: PLL clock */
   __IO uint32_t DEBUG_CLK_EN;                           // 0x2004
   __IO uint32_t DEBUG_CLK_SEL;                          // 0x2008
   __IO uint32_t DEBUG_CLK_DIV;                          // 0x200C
-  uint32_t RESERVED2[1021];
+  uint32_t RESERVED2[1020];
   __IO uint32_t FBUS_CLK_DIV;                           // 0x3000
   __IO uint32_t UNCORE_CLK_DIV;                         // 0x3004
   __IO uint32_t TILE0_CLK_DIV;                          // 0x3008
   __IO uint32_t TILE1_CLK_DIV;                          // 0x300C
   __IO uint32_t TILE2_CLK_DIV;                          // 0x3010
   __IO uint32_t TILE3_CLK_DIV;                          // 0x3014
-  __IO uint32_t TILE4_CLK_DIV;                          // 0x3018
 } RCC_TypeDef;
 
 typedef struct {
--- robo23/inc/robo23.h
+++ robo23/inc/robo23.h
@@ -91,9 +91,7 @@ typedef struct {
   __IO uint32_t MSIP0;                          /** MSIP Registers (1 bit wide) */
   __IO uint32_t MSIP1;                          /** MSIP Registers (1 bit wide) */
   __IO uint32_t MSIP2;                          /** MSIP Registers (1 bit wide) */
-  __IO uint32_t MSIP3;                          /** MSIP Registers (1 bit wide) */
-  __IO uint32_t MSIP4;                          /** MSIP Registers (1 bit wide) */
-  uint32_t RESERVED0[4091];
+  uint32_t RESERVED0[4093];
   __IO uint64_t MTIMECMP0;                      /** MTIMECMP Registers */
   uint32_t RESERVED1[8188];
   __IO uint64_t MTIME;                          /** Timer Register */
@@ -120,28 +118,19 @@ typedef struct {
 typedef struct {
   __IO uint32_t TILE0_RESET;      /** RocketTile 0 */   // 0x00
   __IO uint32_t TILE1_RESET;      /** RocketTile 1 */   // 0x04
-  __IO uint32_t TILE2_RESET;      /** RocketTile 2 */   // 0x08
-  __IO uint32_t TILE3_RESET;      /** RocketTile 3 */   // 0x0C
-  __IO uint32_t TILE4_RESET;      /** SaturnTile 0 */   // 0x10
-  uint32_t RESERVED0[1021];
-  __IO uint32_t TILE0_CLKGATE;    /** RocketTile 0 */   // 0x1008
-  __IO uint32_t TILE1_CLKGATE;    /** RocketTile 1 */   // 0x100C
-  __IO uint32_t TILE2_CLKGATE;    /** RocketTile 2 */   // 0x1010
-  __IO uint32_t TILE3_CLKGATE;    /** RocketTile 3 */   // 0x1014
-  __IO uint32_t TILE4_CLKGATE;    /** SaturnTile 4 */   // 0x1018
-  uint32_t RESERVED1[1017];
-  __IO uint32_t CLK_SEL;          /** Selects the system clock. 0: External slow clock, 1: PLL clock */
+  uint32_t RESERVED0[1022];
+  __IO uint32_t TILE0_CLKGATE;    /** RocketTile 0 */   // 0x1000
+  __IO uint32_t TILE1_CLKGATE;    /** RocketTile 1 */   // 0x1004
+  uint32_t RESERVED1[1023];
+  __IO uint32_t CLK_SEL;          /** Selects the system clock. 0: External slow clock, 1: PLL clock */ 
   __IO uint32_t DEBUG_CLK_EN;                           // 0x2004
   __IO uint32_t DEBUG_CLK_SEL;                          // 0x2008
   __IO uint32_t DEBUG_CLK_DIV;                          // 0x200C
-  uint32_t RESERVED2[1021];
+  uint32_t RESERVED2[1020];
   __IO uint32_t FBUS_CLK_DIV;                           // 0x3000
   __IO uint32_t UNCORE_CLK_DIV;                         // 0x3004
   __IO uint32_t TILE0_CLK_DIV;                          // 0x3008
   __IO uint32_t TILE1_CLK_DIV;                          // 0x300C
-  __IO uint32_t TILE2_CLK_DIV;                          // 0x3010
-  __IO uint32_t TILE3_CLK_DIV;                          // 0x3014
-  __IO uint32_t TILE4_CLK_DIV;                          // 0x3018
 } RCC_TypeDef;
 
 typedef struct {
