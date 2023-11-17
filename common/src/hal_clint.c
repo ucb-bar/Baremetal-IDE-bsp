0545f7c03edd98659cd79d831435c8d5bb187c4e
-T.K.- <t_k_233@outlook.com>
2023-11-17 10:26:26 -0800

    FIX: fix CLINT logic

--- common/src/hal_clint.c
+++ common/src/hal_clint.c
@@ -11,19 +11,27 @@
 #include "hal_clint.h"
 
 uint64_t HAL_CLINT_getTime(CLINT_TypeDef *CLINTx) {
+  #if __riscv_xlen == 32
   uint32_t time_lo;
   uint32_t time_hi;
 
   do {
-    time_hi = *((volatile uint32_t *)(&CLINTx->MTIME) + 1);
-    time_lo = *((volatile uint32_t *)(&CLINTx->MTIME));
-  } while (*((volatile uint32_t *)(&CLINTx->MTIME) + 1) != time_hi);
+    time_hi = *((__IO uint32_t *)(&CLINTx->MTIME) + 1);
+    time_lo = *((__IO uint32_t *)(&CLINTx->MTIME));
+  } while (*((__IO uint32_t *)(&CLINTx->MTIME) + 1) != time_hi);
 
   return (((uint64_t)time_hi) << 32U) | time_lo;
+  #else
+  return CLINTx->MTIME;
+  #endif
 }
 
 void HAL_CLINT_setTimerInterruptTarget(CLINT_TypeDef *CLINTx, uint32_t hartid, uint64_t time) {
-  *((volatile uint32_t *)(&CLINTx->MTIMECMP0) + 4 * hartid + 1) = 0xffffffff;
-  *((volatile uint32_t *)(&CLINTx->MTIMECMP0) + 4 * hartid) = (uint32_t)time;
-  *((volatile uint32_t *)(&CLINTx->MTIMECMP0) + 4 * hartid + 1) = (uint32_t)(time >> 32);
+  #if __riscv_xlen == 32
+  *((__IO uint32_t *)(&CLINTx->MTIMECMP[hartid] + 1)) = 0xFFFFFFFF;
+  *((__IO uint32_t *)(&CLINTx->MTIMECMP[hartid])) = (uint32_t)time;
+  *((__IO uint32_t *)(&CLINTx->MTIMECMP[hartid])) = (uint32_t)(time >> 32);
+  #else
+  CLINTx->MTIMECMP[hartid] = time;
+  #endif
 }
