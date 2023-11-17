aa226693aa15ca2ec46845ad1e50c608d85b3367
-T.K.- <t_k_233@outlook.com>
2023-11-17 10:00:27 -0800

    ADD: update EagleX to new directory structure

--- eaglex/inc/hal_rcc.h
+++ eaglex/inc/hal_rcc.h
@@ -1,21 +1,12 @@
-/**
- * @file hal_rcc.h
- * @author -T.K.- / t_k_233@outlook.com
- * @brief 
- * @version 0.1
- * 
- * @copyright Copyright (c) 2023
- * 
- */
-
-#ifndef __EAGLEX_HAL_RCC_H
-#define __EAGLEX_HAL_RCC_H
+#ifndef __HAL_RCC_H
+#define __HAL_RCC_H
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#include "eaglex_hal.h"
+#include "ll_rcc.h"
+
 
 static inline void HAL_RCC_disableCluster(uint32_t cluster_id) {
   SET_BITS(RCC->CLUSTER_RESETS, 1U << cluster_id);
@@ -32,4 +23,4 @@ void HAL_RCC_initSystemClock();
 }
 #endif
 
-#endif /* __EAGLEX_HAL_RCC_H */
+#endif /* __HAL_RCC_H */
