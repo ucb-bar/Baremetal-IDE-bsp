e6a4f3abc98993c060c33240c9f906a72cc71e7c
-T.K.- <t_k_233@outlook.com>
2023-12-03 23:41:00 -0800

    FIX: fix code style, add TODOs

--- common/inc/hal_pwm.h
+++ common/inc/hal_pwm.h
@@ -13,53 +13,53 @@ typedef struct {
 } PWM_InitTypeDef;
 
 static inline void HAL_PWM_enable(PWM_TypeDef *PWMx) {
-  SET_BITS(pwm->PWM_CFG, PWM_PWMENALWAYS_MSK);
+  SET_BITS(PWMx->PWM_CFG, PWM_PWMENALWAYS_MSK);
 }
 
 static inline void HAL_PWM_disable(PWM_TypeDef *PWMx) {
-  CLEAR_BITS(pwm->PWM_CFG, PWM_PWMENALWAYS_MSK);
+  CLEAR_BITS(PWMx->PWM_CFG, PWM_PWMENALWAYS_MSK);
 }
 
-static inline void HAL_PWM_set_pwm_scale(PWM_TypeDef *PWMx, uint32_t value) {
-  WRITE_BITS(pwm->PWM_CFG, PWM_PWMSCALE_MSK, value << PWM_PWMSCALE_POS);
+static inline void HAL_PWM_setScale(PWM_TypeDef *PWMx, uint32_t value) {
+  WRITE_BITS(PWMx->PWM_CFG, PWM_PWMSCALE_MSK, value << PWM_PWMSCALE_POS);
 }
 
-static inline void HAL_PWM_set_cmp_val(PWM_TypeDef *PWMx, uint32_t idx,
+static inline void HAL_PWM_setCompareValue(PWM_TypeDef *PWMx, uint32_t idx,
                                        uint32_t value) {
   switch (idx) {
   case 0:
-    pwm->PWM_CMP0 = value;
+    PWMx->PWM_CMP0 = value;
     break;
   case 1:
-    pwm->PWM_CMP1 = value;
+    PWMx->PWM_CMP1 = value;
     break;
   case 2:
-    pwm->PWM_CMP2 = value;
+    PWMx->PWM_CMP2 = value;
     break;
   case 3:
-    pwm->PWM_CMP3 = value;
+    PWMx->PWM_CMP3 = value;
     break;
   }
 }
 
 static inline void HAL_PWM_trigger_oneshot(PWM_TypeDef *PWMx, uint32_t idx) {
-  SET_BITS(pwm->PWM_CFG, PWM_PWMENONESHOT_MSK);
+  SET_BITS(PWMx->PWM_CFG, PWM_PWMENONESHOT_MSK);
 }
 
 void HAL_PWM_init(PWM_TypeDef *PWMx, PWM_InitTypeDef *PWM_init);
+
+void HAL_PWM_stop(PWM_TypeDef *PWMx, uint32_t idx);
 
 void HAL_PWM_setFrequency(PWM_TypeDef *PWMx, uint32_t idx, uint32_t freq);
 
 uint32_t HAL_PWM_getFrequency(PWM_TypeDef *PWMx, uint32_t idx);
 
-void HAL_PWM_setDutyCycle(PWM_TypeDef *PWMx, uint32_t idx, uint32_t duty,
-                      int phase_corr);
+void HAL_PWM_setDutyCycle(PWM_TypeDef *PWMx, uint32_t idx, uint32_t duty, int phase_corr);
 
 uint32_t HAL_PWM_getDutyCycle(PWM_TypeDef *PWMx, uint32_t idx);
 
 void HAL_PWM_trigger(PWM_TypeDef *PWMx, uint32_t idx);
 
-void HAL_PWM_stop(PWM_TypeDef *PWMx, uint32_t idx);
 
 #ifdef __cplusplus
 }
--- common/src/hal_pwm.c
+++ common/src/hal_pwm.c
@@ -1,60 +1,61 @@
 /**
  * @file hal_pwm.c
  * @author -Ethan Gao / eygao@berkeley.edu
- * @brief 
+ * @brief
  * @version 0.1
- * 
+ *
  * @copyright Copyright (c) 2023
- * 
+ *
  */
 
 #include "hal_pwm.h"
 
 void HAL_PWM_init(PWM_TypeDef *PWMx) {
-    HAL_PWM_set_pwm_scale(pwm, 0x0);
-    CLEAR_BITS(PWMx->PWM_CFG, PWM_PWMSTICKY_MSK);
-    CLEAR_BITS(PWMx->PWM_CFG, PWM_PWMZEROCMP_MSK);
-    SET_BITS(PWMx->PWM_CFG, PWM_PWMDEGLITCH_MSK);
-    CLEAR_BITS(PWMx->PWM_CFG, PWM_PWMCMP0CENTER_MSK);
-    CLEAR_BITS(PWMx->PWM_CFG, PWM_PWMCMP1CENTER_MSK);
-    CLEAR_BITS(PWMx->PWM_CFG, PWM_PWMCMP2CENTER_MSK);
-    CLEAR_BITS(PWMx->PWM_CFG, PWM_PWMCMP3CENTER_MSK);
-    CLEAR_BITS(PWMx->PWM_CFG, PWM_PWMCMP0GANG_MSK);
-    CLEAR_BITS(PWMx->PWM_CFG, PWM_PWMCMP1GANG_MSK);
-    CLEAR_BITS(PWMx->PWM_CFG, PWM_PWMCMP2GANG_MSK);
-    CLEAR_BITS(PWMx->PWM_CFG, PWM_PWMCMP3GANG_MSK);
-    CLEAR_BITS(PWMx->PWM_CFG, PWM_PWMCMP0IP_MSK);
-    CLEAR_BITS(PWMx->PWM_CFG, PWM_PWMCMP1IP_MSK);
-    CLEAR_BITS(PWMx->PWM_CFG, PWM_PWMCMP2IP_MSK);
-    CLEAR_BITS(PWMx->PWM_CFG, PWM_PWMCMP3IP_MSK);
+  HAL_PWM_setScale(PWMx, 0x0);
+  CLEAR_BITS(PWMx->PWM_CFG, PWM_PWMSTICKY_MSK);
+  CLEAR_BITS(PWMx->PWM_CFG, PWM_PWMZEROCMP_MSK);
+  SET_BITS(PWMx->PWM_CFG, PWM_PWMDEGLITCH_MSK);
+  CLEAR_BITS(PWMx->PWM_CFG, PWM_PWMCMP0CENTER_MSK);
+  CLEAR_BITS(PWMx->PWM_CFG, PWM_PWMCMP1CENTER_MSK);
+  CLEAR_BITS(PWMx->PWM_CFG, PWM_PWMCMP2CENTER_MSK);
+  CLEAR_BITS(PWMx->PWM_CFG, PWM_PWMCMP3CENTER_MSK);
+  CLEAR_BITS(PWMx->PWM_CFG, PWM_PWMCMP0GANG_MSK);
+  CLEAR_BITS(PWMx->PWM_CFG, PWM_PWMCMP1GANG_MSK);
+  CLEAR_BITS(PWMx->PWM_CFG, PWM_PWMCMP2GANG_MSK);
+  CLEAR_BITS(PWMx->PWM_CFG, PWM_PWMCMP3GANG_MSK);
+  CLEAR_BITS(PWMx->PWM_CFG, PWM_PWMCMP0IP_MSK);
+  CLEAR_BITS(PWMx->PWM_CFG, PWM_PWMCMP1IP_MSK);
+  CLEAR_BITS(PWMx->PWM_CFG, PWM_PWMCMP2IP_MSK);
+  CLEAR_BITS(PWMx->PWM_CFG, PWM_PWMCMP3IP_MSK);
 
-    WRITE_BITS(PWMx->PWM_CMP0, PWM_CMP_REG_MSK, 0x0);
-    WRITE_BITS(PWMx->PWM_CMP1, PWM_CMP_REG_MSK, 0x0);
-    WRITE_BITS(PWMx->PWM_CMP2, PWM_CMP_REG_MSK, 0x0);
-    WRITE_BITS(PWMx->PWM_CMP3, PWM_CMP_REG_MSK, 0x0);
-
-}
-
-// void HAL_PWM_set_freq(PWM_TypeDef* pwm, uint32_t idx, uint32_t freq) {
-
-// }
-
-// uint32_t HAL_PWM_setFrequency(PWM_TypeDef* pwm, uint32_t idx) {
-//     return 0;
-// }
-
-// void HAL_PWM_setDutyCycle(PWM_TypeDef* pwm, uint32_t idx, uint32_t duty, int phase_corr) {
-
-// }
-
-// uint32_t HAL_PWM_getDutyCycle(PWM_TypeDef* pwm, uint32_t idx) {
-//     return 0;
-// }
-
-void HAL_PWM_trigger(PWM_TypeDef *PWMx, uint32_t idx) {
-    
+  WRITE_BITS(PWMx->PWM_CMP0, PWM_CMP_REG_MSK, 0x0);
+  WRITE_BITS(PWMx->PWM_CMP1, PWM_CMP_REG_MSK, 0x0);
+  WRITE_BITS(PWMx->PWM_CMP2, PWM_CMP_REG_MSK, 0x0);
+  WRITE_BITS(PWMx->PWM_CMP3, PWM_CMP_REG_MSK, 0x0);
 }
 
 void HAL_PWM_stop(PWM_TypeDef *PWMx, uint32_t idx) {
+  // TODO: implementation
+}
 
+void HAL_PWM_setFrequency(PWM_TypeDef *PWMx, uint32_t idx, uint32_t freq) {
+  // TODO: implementation
+}
+
+uint32_t HAL_PWM_getFrequency(PWM_TypeDef *PWMx, uint32_t idx) {
+  // TODO: implementation
+  return 0;
+}
+
+void HAL_PWM_setDutyCycle(PWM_TypeDef *PWMx, uint32_t idx, uint32_t duty, int phase_corr) {
+  // TODO: implementation
+}
+
+uint32_t HAL_PWM_getDutyCycle(PWM_TypeDef *PWMx, uint32_t idx) {
+  // TODO: implementation
+  return 0; 
+}
+
+void HAL_PWM_trigger(PWM_TypeDef *PWMx, uint32_t idx) {
+  // TODO: implementation
 }
